72515ba5cb80ac2774ad5fb1ad6380fa8b9af0e6
Paul Mabileau <paulmabileau@hotmail.fr>
2022-11-12 03:09:33 +0100

    Feat: Implement basic CLI structure for the cargo subcommand
    Signed-off-by: Paul Mabileau <paulmabileau@hotmail.fr>

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,16 @@
-fn main() {}
+use clap::Parser;
+
+#[derive(clap::Parser, Debug)]
+#[command(name = "cargo")]
+#[command(bin_name = "cargo")]
+enum Cargo {
+    Liner(Liner),
+}
+
+#[derive(clap::Args, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Liner {}
+
+fn main() {
+    dbg!(Cargo::parse());
+}
