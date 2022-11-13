6f0e4f2af43e25090c120c0f5d0a298d22aa9416
Paul Mabileau <paulmabileau@hotmail.fr>
2022-11-13 03:16:44 +0100

    Chore: Fix some clippy warnings
    The tool is actually useful, wow! Who would have thought?
    
    Signed-off-by: Paul Mabileau <paulmabileau@hotmail.fr>

--- src/cargo.rs
+++ src/cargo.rs
@@ -5,17 +5,17 @@ use anyhow::Result;
 
 use crate::config::{Package, UserConfig};
 
-fn install<'a>(name: &'a str, version: &'a str) -> Result<()> {
+fn install(name: &str, version: &str) -> Result<()> {
     Command::new(env::var("CARGO")?)
         .args(["install", "--version", version, name])
         .status()?;
     Ok(())
 }
 
-pub fn install_all<'a>(config: &'a UserConfig) -> Result<()> {
-    for (name, value) in config.packages.iter() {
+pub fn install_all(config: &UserConfig) -> Result<()> {
+    for (name, value) in &config.packages {
         match value {
-            Package::Simple(version) => install(&name, &version.to_string())?,
+            Package::Simple(version) => install(name, &version.to_string())?,
         }
     }
     Ok(())
