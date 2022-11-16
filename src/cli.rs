89b6fa0f779b92c4c1271cd406b2968846a9a24f
Paul Mabileau <paulmabileau@hotmail.fr>
2022-11-16 22:35:13 +0100

    Test: Add some basics tests for cli
    Signed-off-by: Paul Mabileau <paulmabileau@hotmail.fr>

--- src/cli.rs
+++ src/cli.rs
@@ -7,7 +7,7 @@ use clap::Parser;
 /// Cargo entry point for `cargo-liner`.
 ///
 /// This tool is meant to be called using `cargo liner`.
-#[derive(clap::Parser, Debug)]
+#[derive(clap::Parser, Debug, PartialEq, Eq)]
 #[command(name = "cargo")]
 #[command(bin_name = "cargo")]
 enum Cargo {
@@ -16,7 +16,7 @@ enum Cargo {
 }
 
 // The actual entry point in this tool's argument parser.
-#[derive(clap::Args, Debug)]
+#[derive(clap::Args, Debug, PartialEq, Eq)]
 #[command(author, version, about, long_about)]
 pub struct Liner {}
 
@@ -28,5 +28,28 @@ pub struct Liner {}
 pub fn parse_args() -> Liner {
     match Cargo::parse() {
         Cargo::Liner(args) => args,
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_help_iserr() {
+        assert!(Cargo::try_parse_from(["cargo", "liner", "--help"].into_iter()).is_err());
+    }
+
+    #[test]
+    fn test_version_iserr() {
+        assert!(Cargo::try_parse_from(["cargo", "liner", "--version"].into_iter()).is_err());
+    }
+
+    #[test]
+    fn test_no_args() {
+        assert_eq!(
+            Cargo::try_parse_from(["cargo", "liner"].into_iter()).unwrap(),
+            Cargo::Liner(Liner {}),
+        );
     }
 }
