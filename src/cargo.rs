8834d216aede11d42b20a9234862bc6f17cb8b86
Paul Mabileau <paulmabileau@hotmail.fr>
2023-06-28 01:44:22 +0200

    Refactor: Spawn searches in parallel
    Signed-off-by: Paul Mabileau <paulmabileau@hotmail.fr>

--- src/cargo.rs
+++ src/cargo.rs
@@ -5,7 +5,7 @@
 use std::collections::{BTreeMap, BTreeSet};
 use std::env;
 use std::ffi::OsStr;
-use std::process::Command;
+use std::process::{Child, Command, Stdio};
 use std::str;
 
 use anyhow::{anyhow, Result};
@@ -76,14 +76,24 @@ pub fn install_all(
     Ok(())
 }
 
-/// Runs `cargo search` for the given package and returns its latest version.
-pub fn search_exact(pkg: &str) -> Result<Version> {
+/// Spawns `cargo search` for the given package with only stdout piped and
+/// returns the corresponding child process handle to be used with
+/// [`finish_search_exact`].
+fn spawn_search_exact(pkg: &str) -> Result<Child> {
     let mut cmd = Command::new(env::var("CARGO")?);
+    cmd.stdin(Stdio::null());
+    cmd.stderr(Stdio::null());
+    cmd.stdout(Stdio::piped());
     cmd.args(["search", "--limit=1", pkg]);
     log_cmd(&cmd);
+    Ok(cmd.spawn()?)
+}
 
-    let out = str::from_utf8(cmd.output()?.stdout.as_slice())?.to_owned();
-    trace!("Got: {:?}", out);
+/// Waits for the given child process as spawned by [`spawn_search_exact`] to
+/// finish and extract the received package version from the output.
+fn finish_search_exact(pkg: &str, proc: Child) -> Result<Version> {
+    let out = str::from_utf8(proc.wait_with_output()?.stdout.as_slice())?.to_owned();
+    trace!("Search for {:?} got: {:?}", pkg, out);
 
     let ver = Regex::new(&format!(r#"{pkg}\s=\s"([0-9.abrc]+)"\s+#.*"#))?
         .captures(
@@ -103,14 +113,22 @@ pub fn search_exact(pkg: &str) -> Result<Version> {
     Ok(ver)
 }
 
-/// Runs [`search_exact`] for all packages in the given map and returns the
+/// Runs `*_search_exact` for all packages in the given map and returns the
 /// thus fetched versions in the collected map.
 pub fn search_exact_all(pkgs: &BTreeMap<String, Package>) -> Result<BTreeMap<String, Version>> {
     info!("Fetching latest package versions...");
+    let mut procs = Vec::new();
     let mut vers = BTreeMap::new();
 
+    debug!("Spawning search child processes in parallel...");
     for pkg in pkgs.keys() {
-        vers.insert(pkg.clone(), search_exact(pkg)?);
+        procs.push(spawn_search_exact(pkg)?);
+    }
+
+    debug!("Waiting for each search child processes to finish...");
+    // Key traversal order is stable because sorted.
+    for (pkg, proc) in pkgs.keys().zip(procs.into_iter()) {
+        vers.insert(pkg.clone(), finish_search_exact(pkg, proc)?);
     }
 
     Ok(vers)
