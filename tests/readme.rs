ea2c4514e265720994d33cf0e6e766dbf2888884
Paul Mabileau <paulmabileau@hotmail.fr>
2024-01-19 21:38:30 +0100

    Test(README): Retrict output width to 70 columns
    Even less manual intervention.
    
    Signed-off-by: Paul Mabileau <paulmabileau@hotmail.fr>

--- tests/readme.rs
+++ tests/readme.rs
@@ -9,6 +9,11 @@ use trycmd::TestCases;
 fn validate_readme() {
     TestCases::new()
         .register_bin("cargo", trycmd::cargo::cargo_bin!(clap::crate_name!()))
+        // Restrict the output's width by emulating a narrower terminal in
+        // order for the file to be kept properly-formatted. 70 columns should
+        // make the output fit in the current rendering of crates.io for code
+        // blocks.
+        .env("COLUMNS", "70")
         .case("README.md")
         .run();
 }
