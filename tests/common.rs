45e0af3a68d4db157764b8cca2312795741cf2f5
Paul Mabileau <paulmabileau@hotmail.fr>
2024-01-23 23:06:07 +0100

    Test: Add common module
    Will be used by most integration tests.
    
    Signed-off-by: Paul Mabileau <paulmabileau@hotmail.fr>

---
+++ tests/common.rs
@@ -0,0 +1,95 @@
+use std::collections::HashMap;
+
+use cargo_test_support::{
+    compare,
+    registry::{HttpServer, RegistryBuilder, Request, Response, TestRegistry},
+    TestEnv,
+};
+use snapbox::cmd::Command;
+
+/// Invoke `cargo-liner liner` with the test environment.
+#[allow(unused)]
+#[must_use]
+pub fn cargo_liner() -> Command {
+    Command::new(snapbox::cmd::cargo_bin(clap::crate_name!()))
+        .with_assert(compare::assert_ui())
+        .test_env()
+        .arg("liner")
+        .arg("--color=never")
+}
+
+/// Initializes the registry without a token, with an HTTP index and API, and
+/// with a custom API responder that mimics `cargo search`'s endpoint.
+///
+/// Returns a test registry handle that must be saved in order for the test
+/// server to be kept alive.
+#[allow(clippy::missing_panics_doc)]
+#[must_use]
+pub fn init_registry() -> TestRegistry {
+    // Must be fetched ahead of time, otherwise the underlying global state is
+    // uninitialized for some reason, very probably because the request handler
+    // below is ran in another thread and the global state is thread-local as
+    // each thread normally corresponds to a different test ran in parallel of
+    // others.
+    let test_root = cargo_test_support::paths::root();
+    RegistryBuilder::new()
+        .no_configure_token()
+        .http_api()
+        .http_index()
+        .add_responder("/api/v1/crates", move |req: &Request, _srv: &HttpServer| {
+            let req_params = req
+                .url
+                .query_pairs()
+                .map(|(k, v)| (k.into_owned(), v.into_owned()))
+                .collect::<HashMap<_, _>>();
+            // Search mandatory parameter: string query.
+            let req_pkg = req_params.get("q").unwrap();
+            let dl_path = test_root.join("dl");
+
+            // Use the `dl` directory instead of the `registry` one in order to
+            // easily test whether a packaged has previously been published or
+            // not: test if a sub-directory of the package's name exists.
+            match dl_path.join(req_pkg).read_dir() {
+                Ok(mut dir) => {
+                    let pkg_ver = dir
+                        .next()
+                        .unwrap()
+                        .unwrap()
+                        .file_name()
+                        .into_string()
+                        .unwrap();
+                    Response {
+                        code: 200,
+                        headers: Vec::new(),
+                        body: format!(
+                            // Return only the information actually required by
+                            // the `cargo search` command. Might need to be
+                            // extended in the future if more gets required.
+                            r#"{{
+                                "crates": [{{
+                                    "name": "{req_pkg}",
+                                    "description": "whatever",
+                                    "newest_version": "{pkg_ver}",
+                                    "max_version": "{pkg_ver}"
+                                }}],
+                                "meta": {{
+                                    "next_page": "?q={}&page=2",
+                                    "prev_page": null,
+                                    "total": 1
+                                }}
+                            }}"#,
+                            req.url.query().unwrap(),
+                        )
+                        .as_bytes()
+                        .to_vec(),
+                    }
+                }
+                Err(err) => Response {
+                    code: 404,
+                    headers: Vec::new(),
+                    body: format!("{err:?}").as_bytes().to_vec(),
+                },
+            }
+        })
+        .build()
+}
