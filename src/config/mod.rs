e6c851d341ba4c307a0a108323dee2cbc81ca4ef
Paul Mabileau <paulmabileau@hotmail.fr>
2025-02-21 23:31:24 +0100

    Chore: Update to Clippy 1.85
    Signed-off-by: Paul Mabileau <paulmabileau@hotmail.fr>

--- src/config/mod.rs
+++ src/config/mod.rs
@@ -2,10 +2,6 @@
 //!
 //! See [`UserConfig::parse_file`] in order to retrieve such configuration
 //! settings from the default file.
-#![expect(
-    clippy::module_name_repetitions,
-    reason = "Sub-parts of the global configuration are still configuration in themselves."
-)]
 
 mod cargo_crates_toml;
 mod effective_config;
