3de65951a20a49327deb9cf4c364c5044991c3a4
Paul Mabileau <paulmabileau@hotmail.fr>
2025-06-14 23:55:28 +0200

    Feat(jettison): Log uninstallation plan before executing it
    As part of #28.
    
    Signed-off-by: Paul Mabileau <paulmabileau@hotmail.fr>

--- src/commands/jettison.rs
+++ src/commands/jettison.rs
@@ -4,9 +4,11 @@ use clap::ColorChoice;
 use color_eyre::Result;
 use color_eyre::eyre::Context;
 use semver::Version;
+use tabled::Tabled;
 
 use crate::cargo;
 use crate::cli::JettisonArgs;
+use crate::commands::styled_table;
 use crate::config::{CargoCratesToml, PackageRequirement, UserConfig};
 
 pub fn run(_args: &JettisonArgs, cargo_color: ColorChoice, cargo_verbosity: i8) -> Result<()> {
@@ -15,6 +17,8 @@ pub fn run(_args: &JettisonArgs, cargo_color: ColorChoice, cargo_verbosity: i8) 
         CargoCratesToml::parse_file().wrap_err("Failed to parse Cargo's .crates.toml file.")?;
 
     let to_uninstall = needing_uninstall(cct.into_name_versions(), &config.packages);
+    log_uninstallation_plan(&to_uninstall);
+
     cargo::uninstall_all(to_uninstall.into_keys(), cargo_color, cargo_verbosity)
         .wrap_err("Some package failed to uninstall.")?;
     Ok(())
@@ -26,10 +30,35 @@ fn needing_uninstall(
     mut installed: BTreeMap<String, Version>,
     configured: &BTreeMap<String, PackageRequirement>,
 ) -> BTreeMap<String, Version> {
+    log::debug!("Computing uninstallation plan...");
     // Always exclude self from uninstallation.
     installed.remove(clap::crate_name!());
     installed
         .into_iter()
         .filter(|(pkg, _)| !configured.contains_key(pkg))
         .collect()
+}
+
+/// Logs the packages that will be removed.
+fn log_uninstallation_plan(to_uninstall: &BTreeMap<String, Version>) {
+    if to_uninstall.is_empty() {
+        log::info!("No package to uninstall: all installed are configured as well.");
+    } else {
+        log::info!(
+            "Will uninstall:\n{}",
+            styled_table(to_uninstall.iter().map(|(pkg, ver)| PackageEntry {
+                name: pkg.clone(),
+                version: ver.to_string(),
+            })),
+        );
+    }
+}
+
+/// [`Tabled`] for logging packages.
+#[derive(Tabled)]
+struct PackageEntry {
+    #[tabled(rename = "Name")]
+    name: String,
+    #[tabled(rename = "Version")]
+    version: String,
 }
