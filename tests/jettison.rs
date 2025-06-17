c5e7c8f3b8173fbbdf91600e32e88fbe4388603c
Paul Mabileau <paulmabileau@hotmail.fr>
2025-06-17 13:29:32 +0200

    Test(jettison): Cover confirmation
    As part of #28.
    
    Signed-off-by: Paul Mabileau <paulmabileau@hotmail.fr>

--- tests/jettison.rs
+++ tests/jettison.rs
@@ -125,3 +125,98 @@ fn validate_jettison_verbosity_and_color() {
         ]);
     assert_not_installed("abc");
 }
+
+#[cargo_test]
+fn validate_jettison_confirm_continue_y() {
+    fake_install_self();
+    fake_install("abc", "0.0.0", false);
+    assert_installed("abc");
+    write_user_config(&["[packages]"]);
+
+    cargo_liner()
+        .arg("jettison")
+        .stdin("y")
+        .assert()
+        .success()
+        .stdout_eq("".into_data().raw())
+        .stderr_eq(snapbox::file![
+            "fixtures/jettison/validate_jettison_confirm_continue.stderr"
+        ]);
+    assert_not_installed("abc");
+}
+
+#[cargo_test]
+fn validate_jettison_confirm_continue_lf() {
+    fake_install_self();
+    fake_install("abc", "0.0.0", false);
+    assert_installed("abc");
+    write_user_config(&["[packages]"]);
+
+    cargo_liner()
+        .arg("jettison")
+        .stdin("")
+        .assert()
+        .success()
+        .stdout_eq("".into_data().raw())
+        .stderr_eq(snapbox::file![
+            "fixtures/jettison/validate_jettison_confirm_continue.stderr"
+        ]);
+    assert_not_installed("abc");
+}
+
+#[cargo_test]
+fn validate_jettison_confirm_abort() {
+    fake_install_self();
+    fake_install("abc", "0.0.0", false);
+    assert_installed("abc");
+    write_user_config(&["[packages]"]);
+
+    cargo_liner()
+        .arg("jettison")
+        .stdin("n")
+        .assert()
+        .success()
+        .stdout_eq("".into_data().raw())
+        .stderr_eq(snapbox::file![
+            "fixtures/jettison/validate_jettison_confirm_abort.stderr"
+        ]);
+    assert_installed("abc");
+}
+
+#[cargo_test]
+fn validate_jettison_confirm_retry_continue() {
+    fake_install_self();
+    fake_install("abc", "0.0.0", false);
+    assert_installed("abc");
+    write_user_config(&["[packages]"]);
+
+    cargo_liner()
+        .arg("jettison")
+        .stdin("a\nb\nc\ny")
+        .assert()
+        .success()
+        .stdout_eq("".into_data().raw())
+        .stderr_eq(snapbox::file![
+            "fixtures/jettison/validate_jettison_confirm_retry_continue.stderr"
+        ]);
+    assert_not_installed("abc");
+}
+
+#[cargo_test]
+fn validate_jettison_confirm_retry_abort() {
+    fake_install_self();
+    fake_install("abc", "0.0.0", false);
+    assert_installed("abc");
+    write_user_config(&["[packages]"]);
+
+    cargo_liner()
+        .arg("jettison")
+        .stdin("a\nb\nc\nn")
+        .assert()
+        .success()
+        .stdout_eq("".into_data().raw())
+        .stderr_eq(snapbox::file![
+            "fixtures/jettison/validate_jettison_confirm_retry_abort.stderr"
+        ]);
+    assert_installed("abc");
+}
